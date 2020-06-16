54eccfa449915b687e6cc4d74dc87e217d8a9d31
rixo <rixo@rixo.fr>
2020-06-17 01:52:04 +0200

    add HMR (+ trojan)

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
-  import './style/main.css';
-  import { Router } from '@sveltech/routify/runtime';
-  import routes from './routes';
+  import './style/main.css'
+  import { Router } from '@sveltech/routify/runtime'
+
+  // NOTE actually I don't think sure Routify supports hot swapping of routes...
+  // would probably need to rerun routing
+  // import routes from './routes';
+  import { routes } from '../.routify/routes'
 </script>
 
-<Router routes={$routes} />
+<Router {routes} />
