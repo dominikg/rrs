59b69d86492eecbfd12e32e8bc17289db2bda4b6
dominikg <dominik.goepel@gmx.de>
2020-06-17 12:19:44 +0200

    resolve duplicate eslint,prettier situation; some cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,7 @@
 <script>
-  import './style/main.css'
-  import { Router } from '@sveltech/routify/runtime'
-
-  // NOTE actually I don't think sure Routify supports hot swapping of routes...
-  // would probably need to rerun routing
-  // import routes from './routes';
-  import { routes } from '../.routify/routes'
+  import './style/main.css';
+  import { Router } from '@sveltech/routify/runtime';
+  import { routes } from '../.routify/routes';
 </script>
 
 <Router {routes} />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import App from './App.svelte'
+import App from './App.svelte';
 
 const app = new App({
   target: document.body,
-})
+});
 
-export default app
+export default app;
 
 // NOTE I'm not sure it is a good idea to accept here, given how I seemed to get
 // weird update bubbling, a full reload might be preferable
